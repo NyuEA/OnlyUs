f7d2a0de84848da6618a68b6f09ebf446a945c2a
ydw <ydw@ydw7188>
2016-09-18 12:39:24 +0900

    많이 고침...zzㅋㅋ

--- OnlyUs/src/com/controller/bisMemberAddServlet.java
+++ OnlyUs/src/com/controller/bisMemberAddServlet.java
@@ -1,20 +1,55 @@
 package com.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.dto.bisUserDTO;
+import com.exception.CommonException;
+import com.service.bizUserService;
+
 /**
  * Servlet implementation class genMemberAddServlet
  */
-@WebServlet("/genMemberAddServlet")
+@WebServlet("/bisMemberAddServlet")
 public class bisMemberAddServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-	}
+		String bisid = request.getParameter("bisid");
+		String passwd = request.getParameter("passwd");
+		String bisname = request.getParameter("bisname");
+		String post1 = request.getParameter("post1");
+		String post2 = request.getParameter("post2");
+		String addr1 = request.getParameter("addr1");
+		String addr2 = request.getParameter("addr2");
+		String phone1 = request.getParameter("phone1");
+		String phone2 = request.getParameter("phone2");
+		String phone3 = request.getParameter("phone3");
+		
+		bisUserDTO dto = new bisUserDTO(bisid, passwd, bisname, Integer.parseInt(post1), Integer.parseInt(post2), addr1, addr2, Integer.parseInt(phone1), Integer.parseInt(phone2), Integer.parseInt(phone3));
+		bizUserService service = new bizUserService();
+		 String title="";
+		    String target="";
+		    try {
+				service.addBisUser(dto);
+				target = "login.jsp";
+				request.setAttribute("result", "success");
+			} catch (CommonException e) {
+				title= e.getMessage();
+				String link="join.jsp";
+				target="error.jsp";
+				request.setAttribute("title", title);
+				request.setAttribute("link", link);
+			}
+			
+			RequestDispatcher dis =
+					request.getRequestDispatcher(target);
+			dis.forward(request, response);
+		}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
---
+++ OnlyUs/src/com/service/bizUserService.java
@@ -0,0 +1,24 @@
+package com.service;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.config.MySqlSessionFactory;
+import com.dto.bisUserDTO;
+import com.exception.CommonException;
+
+public class bizUserService {
+	public void addBisUser(bisUserDTO dto) throws CommonException {
+		SqlSession session = MySqlSessionFactory.getSession();
+		try {
+			int n = session.insert("addBisUser", dto);
+			session.commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new CommonException("회원등록 실패");
+		} finally {
+			session.close();
+		}
+
+	}// end addMember
+}
